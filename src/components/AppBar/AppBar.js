d01853ec40ad1822b32426a85ae085c957527a80
Sergey-Proskurnin <proskurnin@ua.fm>
2021-06-30 15:05:48 +0300

    Add animation header

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import Navigation from 'components/Navigation';
 import s from './AppBar.module.css';
+import headerStyles from './headerAppBar.module.css';
+import navigationStyle from './navigationAppBar.module.css';
 
 const AppBar = () => {
   return (
-    <header className={s.AppBar}>
-      <Navigation />
-    </header>
+    <CSSTransition
+      in={true}
+      appear={true}
+      timeout={500}
+      classNames={headerStyles}
+      unmountOnExit
+    >
+      {stage => {
+        return (
+          <header className={s.AppBar}>
+            <CSSTransition
+              in={stage === 'entered'}
+              timeout={500}
+              classNames={navigationStyle}
+              unmountOnExit
+            >
+              <Navigation />
+            </CSSTransition>
+          </header>
+        );
+      }}
+    </CSSTransition>
   );
 };
 
